b558a5c5c4d0c1c0ae2740cab3ffcd65f9684df0
Guilherme Comin <82686323+GuilhermeComin@users.noreply.github.com>
2023-10-23 17:19:08 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+// Abrir e fechar Menu
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (var element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+    evento
+  })
+}
+// Fechar quando clicar no a
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (var link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+// Shadow after scroll on header
+
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
+window.addEventListener('scroll', function () {
+  if (window.scrollY >= navHeight) {
+    header.classList.add('scroll')
+  } else {
+    header.classList.remove('scroll')
+  }
+})
